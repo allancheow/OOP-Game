39d7d7257d9a4aa5028e310be367f53797b60d4c
Allan Cheow <allan.cheow132@gmail.com>
2021-01-29 21:08:12 -0800

    Completed Part 1 of build

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,27 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+    constructor (phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    /**
+    * Display phrase on game board
+    */
+    addPhraseToDisplay() {
+         // Selects the <ul> within <div> with id=phrase
+        const ul = document.querySelector(`#phrase ul`);
+        for ( let i = 0; i < this.phrase.length; i++ ) {
+            // Creates new <li> element
+            const li = document.createElement(`li`);
+            li.textContent = this.phrase[i];
+            // Used chained ternary operator to set class(es) for each character
+            li.textContent === ` ` ? li.className = `space`
+                : li.textContent === `'` ? li.className = `letter ${this.phrase[i]}` 
+                : li.className = `hide letter ${this.phrase[i]}`;
+            ul.appendChild(li);
+        }
+    }
+ }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+// Initiate start button to clear overlay and start game
+document.querySelector(`#btn__reset`).addEventListener( `click`, e => new Game().startGame() );
