0d641df77a95178aaf89ab102c1b60a15909f375
Allan Cheow <allan.cheow132@gmail.com>
2021-02-02 13:17:55 -0800

    added preliminary code for exceeds

--- js/app.js
+++ js/app.js
@@ -2,5 +2,19 @@
  * Project 4 - OOP Game App
  * app.js */
 
+let game;
+const letters = document.querySelectorAll(`.key`);
+const startBtn = document.querySelector(`#btn__reset`);
+
 // Initiate start button to clear overlay and start game
-document.querySelector(`#btn__reset`).addEventListener( `click`, e => new Game().startGame() );
+startBtn.addEventListener( `click`, e =>{
+    game = new Game();
+    game.startGame() 
+});
+
+// Cycles through the array to listen for any onscreen click 
+letters.forEach( letter => letter.addEventListener( `click`, e => game.handleInteraction(letter)) );
+
+document.addEventListener( `keydown`, e => {
+    letters.forEach( letter => letter.textContent === e.key ? game.handleInteraction(letter) : null );
+});
